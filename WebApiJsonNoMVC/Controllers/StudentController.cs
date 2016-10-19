89365c8cfdeb8b78ef925018be6c0ac59f56c924
Greg Doud <greg.doud@theironyard.com>
2016-10-19 06:16:15 -0400

    Add project files.

---
+++ WebApiJsonNoMVC/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiJsonNoMVC.Models;
+
+namespace WebApiJsonNoMVC.Controllers {
+    public class StudentController : ApiController {
+
+        Student[] students = new Student[] {
+            new Student("Greg", 400, 1.5),
+            new Student("Cindy", 1600, 4.5),
+            new Student("Nick", 1400, 3.5),
+            new Student("Ken", 1400, 3.5)
+        };
+ 
+        // GET api/<controller>
+        public IEnumerable<Student> Get() {
+            return students;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value) {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value) {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id) {
+        }
+    }
+}
---
+++ WebApiJsonNoMVC/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiJsonNoMVC.Models {
+    public class Student {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        int _sat;
+        public int Sat {
+            get {
+                return _sat;
+            }
+            set {
+                _sat = value;
+            }
+        }
+        public double Gpa { get; set; }
+
+        public Student() { }
+        public Student(string name, int sat, double gpa) {
+            Name = name;
+            Sat = sat;
+            Gpa = gpa;
+        }
+    }
+}
