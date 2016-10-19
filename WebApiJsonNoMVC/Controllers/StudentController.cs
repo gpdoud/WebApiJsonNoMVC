4acbf463a535461b863cc733802a3c797ff7e17d
Greg Doud <gpdoud@gmail.com>
2016-10-19 15:13:15 -0400

    Added support for Entity Framework

--- WebApiJsonNoMVC/Controllers/StudentController.cs
+++ WebApiJsonNoMVC/Controllers/StudentController.cs
@@ -22,14 +22,23 @@ namespace WebApiJsonNoMVC.Controllers {
 
         // INSERT: POST api/<controller>
         public void Post([FromBody]Student student) {
+            db.Students.Add(student);
+            db.SaveChanges();
         }
 
         // UPDATE: PUT api/<controller>/5
         public void Put(int id, [FromBody]Student student) {
+            var studDb = db.Students.SingleOrDefault(s => s.Id == student.Id);
+            studDb.Name = student.Name;
+            studDb.Sat = student.Sat;
+            studDb.Gpa = student.Gpa;
+            db.SaveChanges();
         }
 
         // DELETE api/<controller>/5
         public void Delete(int id) {
+            db.Students.Remove(db.Students.SingleOrDefault(s => s.Id == id));
+            db.SaveChanges();
         }
     }
 }
